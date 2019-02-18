650c838d08f8c953d3e91efdf7e8de6f883fc122
mtianyan <1147727180@qq.com>
2019-02-18 13:18:33 +0800

    完成订阅模式编码，暂时并没有推送成功，原因不明

--- hackernews-vue-apollo-mtianyan/src/main.js
+++ hackernews-vue-apollo-mtianyan/src/main.js
@@ -1,5 +1,5 @@
 // 1
-
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
@@ -22,6 +22,14 @@ const httpLink = new HttpLink({
   uri: 'https://api.graph.cool/simple/v1/cjs5snl0w22990114nl9f8sgr'
 })
 
+// 订阅更新客户端
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cjs5snl0w22990114nl9f8sgr', {
+  reconnect: true,
+  connectionParams: {
+    authToken: localStorage.getItem(GC_AUTH_TOKEN)
+  }
+})
+
 // 要放在httpLink 和 ApolloClient之间
 const authMiddleware = new ApolloLink((operation, forward) => {
   // add the authorization to the headers
@@ -35,9 +43,15 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   return forward(operation)
 })
 
+const httpLinkWithSubscriptions = addGraphQLSubscriptions(
+  authMiddleware.concat(httpLink),
+  wsClient
+)
+
+
 // 4
 const apolloClient = new ApolloClient({
-  link: authMiddleware.concat(httpLink),
+  link: httpLinkWithSubscriptions,
   cache: new InMemoryCache(),
   connectToDevTools: true
 })
